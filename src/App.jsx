01061d44620b77dde172ce69b74ab6f029600db2
Ramesh Das <rameshdas@Rameshs-MacBook-Air.local>
2024-12-15 23:07:26 +0530

    Project Almost Done

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import About from './sections/About.jsx';
+import Hero from './sections/Hero.jsx';
+import Navbar from './sections/Navbar.jsx';
+import Projects from './sections/Project.jsx';
+
+
+const App = () => {
+  return (
+    <main className="max-w-7xl mx-auto relative">
+      <Navbar />
+      <Hero/>
+      <About/>
+      <Projects/>
+    </main>
+  );
+};
+
+export default App;
